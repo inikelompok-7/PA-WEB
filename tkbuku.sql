a2bef7bd40f0c86b3c16dc513e3bb7d58ab1a288
rezkyputra16 <rezkyputra827@gmail.com>
2023-05-15 20:55:41 +0800

    database new

--- tkbuku.sql
+++ tkbuku.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2023 at 01:39 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Waktu pembuatan: 15 Bulan Mei 2023 pada 14.53
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buku`
+-- Struktur dari tabel `acc_sup`
+--
+
+CREATE TABLE `acc_sup` (
+  `id_supp` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `acc_sup`
+--
+
+INSERT INTO `acc_sup` (`id_supp`, `username`, `password`) VALUES
+(1, 'supplier', 'supplier123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
 --
 
 CREATE TABLE `buku` (
@@ -34,63 +53,45 @@ CREATE TABLE `buku` (
   `penerbit` varchar(30) NOT NULL,
   `harga` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `buku`
+-- Dumping data untuk tabel `buku`
 --
 
 INSERT INTO `buku` (`id_buku`, `judul`, `pengarang`, `penerbit`, `harga`, `jumlah`) VALUES
-(6, 'test', 'asd', 'qwer', 10000, 97),
+(6, 'test', 'asd', 'qwer', 10000, 70),
 (7, 'aaa', 'asdasd', 'qwer', 12314, 12),
-(8, 'weqweqwe', 'asd', 'qwer', 100, 12);
+(9, 'ganas', 'ganas', 'ganas', 10000, 100);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pegawai`
---
-
-CREATE TABLE `pegawai` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `alamat` varchar(30) NOT NULL,
-  `nohp` varchar(30) NOT NULL,
-  `role` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `pegawai`
---
-
-INSERT INTO `pegawai` (`id_user`, `username`, `password`, `alamat`, `nohp`, `role`) VALUES
-(1, 'admin1', 'admin', 'jl.apa aja', '123456', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `penjualan`
+-- Struktur dari tabel `penjualan`
 --
 
 CREATE TABLE `penjualan` (
   `id_buku` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `total_harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `total_harga` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `penjualan`
+-- Dumping data untuk tabel `penjualan`
 --
 
-INSERT INTO `penjualan` (`id_buku`, `username`, `jumlah`, `total_harga`) VALUES
-(6, 'nana', 2, 20000);
+INSERT INTO `penjualan` (`id_buku`, `username`, `jumlah`, `total_harga`, `tanggal`) VALUES
+(6, 'nana', 2, 20000, '2023-05-15 12:33:46'),
+(6, 'user', 7, 70000, '2023-05-15 12:33:46'),
+(6, 'user', 10, 100000, '2023-05-15 12:33:46'),
+(6, 'user', 10, 100000, '2023-05-15 12:43:09');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -100,73 +101,68 @@ CREATE TABLE `users` (
   `alamat` varchar(30) NOT NULL,
   `nohp` varchar(13) NOT NULL,
   `role` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id_user`, `username`, `password`, `alamat`, `nohp`, `role`) VALUES
+(1, 'admin', 'admin123', 'jl.lempuyangan', '08123456', 'admin'),
+(2, 'supplier', 'supplier123', 'jl.pramuka', '123456', 'supplier'),
 (3, 'user', 'user123', 'jl.apaaja', '2345123', 'user'),
-(7, 'nana', 'nana', 'nana', '123', 'user'),
-(8, 'supplier', 'supplier', 'jl.lempuyangan', '08123456', 'supplier');
+(7, 'nana', 'nana', 'nana', '123', 'user');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `buku`
+-- Indeks untuk tabel `acc_sup`
+--
+ALTER TABLE `acc_sup`
+  ADD PRIMARY KEY (`id_supp`);
+
+--
+-- Indeks untuk tabel `buku`
 --
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`);
 
 --
--- Indexes for table `pegawai`
---
-ALTER TABLE `pegawai`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- Indexes for table `penjualan`
+-- Indeks untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD KEY `FK_buku` (`id_buku`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `buku`
+-- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_buku` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `pegawai`
---
-ALTER TABLE `pegawai`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `penjualan`
+-- Ketidakleluasaan untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD CONSTRAINT `FK_buku` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`);
